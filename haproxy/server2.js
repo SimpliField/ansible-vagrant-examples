5ac455c75ce43c7be03354b6904e01c589f37fb1
Sébastien Elet <sebastien@elet.fr>
2016-06-30 16:13:11 +0200

    feat(haproxy): add vagrant box to test haproxy role

---
+++ haproxy/server2.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  const body = JSON.stringify({
+    server2: new Date()
+  });
+
+  res.setHeader('Content-Length', Buffer.byteLength(body));
+  res.writeHead(200);
+  res.end(body);
+});
+
+server.listen(8001);
